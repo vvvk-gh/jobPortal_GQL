124ddad4361b614698a3b9c22a31e0dc6e945ac3
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-10-31 23:13:24 +0530

    Custom object in GraphQL schema

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,11 @@
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello World!'
+        job: () => {
+            return {
+                id: 'test-id',
+                title: 'Software Devloper',
+                description: 'responsible for designing, coding, testing and deploying software applications that meet user needs and specifications. They use various programming languages, frameworks and tools to create efficient and functional software products.'
+            };
+        }
     }
-};
+}
