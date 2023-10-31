d13f41b8936d4ef6ed1529fb059c8551ddafca5a
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-10-31 21:21:59 +0530

    Apollo server and Graphql endpoint

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+//integrate Apollo server into the express app
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import { readFile } from 'node:fs/promises'
+import { resolvers } from './resolvers.js'
+
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
@@ -9,6 +15,13 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+//integrate Apollo server into the express app
+const typeDefs = await readFile('./schema.graphql', 'utf-8');
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+app.use('/graphql', apolloMiddleware(apolloServer));
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint on http://localhost:${PORT}/graphql`);
 });
