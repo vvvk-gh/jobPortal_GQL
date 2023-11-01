f47e6c1e6375dd686e5cfec2b4e11dc1fc3251aa
vvvk-gh <vamsivutukuri1997@gmail.com>
2023-11-01 16:36:52 +0530

    Field Resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,5 +4,13 @@ import { getJobs } from "./db/jobs.js"
 export const resolvers = {
     Query: {
         jobs: () => getJobs(),
+    },
+
+    Job: {
+        date: (job) => ISOtoDate(job.createdAt),
     }
+}
+
+function ISOtoDate(value) {
+    return value.slice('0', 'yyyy-mm-dd'.length)
 }
